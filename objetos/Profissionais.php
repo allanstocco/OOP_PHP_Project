f36eadd54cb176d8a63ecbda13b0495ef6813776
Allan Stocco <allanstocco@hotmail.com>
2021-07-31 15:41:56 +0100

    incluído o cadastro de profissionais

--- objetos/Profissionais.php
+++ objetos/Profissionais.php
@@ -17,11 +17,31 @@ class Profissionais {
         $this->conn = $db;
     }
 
+    function cadastroProf() {
+        // insere no banco de dados
+        $query = "INSERT INTO " . $this->table_name . " SET name=:name, surname=:surname, especialidade=:especialidade";
+        $stmt = $this->conn->prepare($query);
+
+        $this->name = htmlspecialchars(strip_tags($this->name));
+        $this->surname = htmlspecialchars(strip_tags($this->surname));
+        $this->especialidade = htmlspecialchars(strip_tags($this->especialidade));
+
+        $stmt->bindParam(":name", $this->name);
+        $stmt->bindParam(":surname", $this->surname);
+        $stmt->bindParam(":especialidade", $this->especialidade);
+
+        if ($stmt->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     // USADO PARA SELECIONAR EM DROPDOWN LIST:
     function read() {
-        
+
         //SELECIONA TODO O DATA
-        
+
         $query = "SELECT id, name, surname, especialidade FROM " . $this->table_name . " ORDER BY name";
 
         $stmt = $this->conn->prepare($query);
@@ -32,6 +52,8 @@ class Profissionais {
 
     function readName() {
 
+
+
         $query = "SELECT name, surname, especialidade FROM " . $this->table_name . " WHERE id = ? limit 0,1";
 
         $stmt = $this->conn->prepare($query);
@@ -39,13 +61,9 @@ class Profissionais {
         $stmt->execute();
 
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
-
         $this->name = $row['name'];
         $this->surname = $row['surname'];
         $this->especialidade = $row['especialidade'];
-        
     }
 
 }
-
-?>
